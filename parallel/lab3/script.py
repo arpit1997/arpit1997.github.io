090a807ad5ac48baca2c1c98413ca06633d63b97
arpit1997 <solankiarpit1997@gmail.com>
2019-07-02 02:14:06 +0530

    website revamp :heart

--- parallel/lab3/script.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-s = [5, 10, 100,500,1000,5000,10000]
-n_p = [1,2,4,8,16,32]
-for i in s:
-	for j in n_p:
-		a="time ./matmul_parallel "+str(i)+" "+str(j)
-		subprocess.call(a,shell=True)
